1694a893c6e67a85ab56915f872169890ec414bf
farley0909 <jose.farley@academico.ifpb.edu.br>
2022-10-16 18:45:50 -0300

    atualizando config cors

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ const dadosFuncionarios = require('./routes/rota-dadosFuncionario.js')
 const atualizar = require('./routes/rota-atualizar.js') 
 const port = process.env.PORT || 8080
 const app = express()
-app.use(cors())
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 //Configurando o bodyParser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
