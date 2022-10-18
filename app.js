c56a930a88bbe487c78b163dcfb89e15c7535e72
jose.farley <jose.farley@academico.ifpb.edu.br>
2022-10-18 16:38:32 -0300

    resolvendo cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const path = require("path")
-//const cors = require('cors')
+const cors = require('cors')
 const bodyParser = require("body-parser")
 const express = require("express")
 const login = require('./routes/rota-login.js') 
@@ -14,7 +14,14 @@ const dadosFuncionarios = require('./routes/rota-dadosFuncionario.js')
 const atualizar = require('./routes/rota-atualizar.js') 
 const port = process.env.PORT || 8080
 const app = express()
-
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 //Configurando o bodyParser
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
