0290fd751bcb001ff67d1006d0fb163146273b99
jose.farley <jose.farley@academico.ifpb.edu.br>
2022-10-18 14:34:50 -0300

    resolvendo cors

--- routes/rota-dadosFuncionario.js
+++ routes/rota-dadosFuncionario.js
@@ -3,13 +3,16 @@ const cors = require('cors');
 const path = require('path')
 const router = express.Router()
 const FuncionarioDAO = require("../dist/DAO/FuncionarioDAO.js")
-router.use(cors())
 
 router.get("/dadosFuncionarios/:token", async (req, res)=>{
     const funcionario =new  FuncionarioDAO()
     const data = {
         data: await funcionario.mostrar()
     }
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
     res.json(data)
 })
 module.exports = router
