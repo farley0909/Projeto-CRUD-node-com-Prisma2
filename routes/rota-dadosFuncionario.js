a8a1d40d10890380fe5aef8e1d8320e7c9b1380e
jose.farley <jose.farley@academico.ifpb.edu.br>
2022-10-18 14:50:45 -0300

    resolvendo cors

--- routes/rota-dadosFuncionario.js
+++ routes/rota-dadosFuncionario.js
@@ -12,7 +12,6 @@ router.get("/dadosFuncionarios/:token", async (req, res)=>{
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
     res.json(data)
 })
 module.exports = router
